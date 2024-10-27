06b9118ebea9f94a353b697351ac5dc3c52edb3b
varandystany <130069568+varandystany@users.noreply.github.com>
2024-10-27 10:48:39 +0300

    Update FactoryClasses.py

--- FactoryClasses.py
+++ FactoryClasses.py
@@ -1,11 +1,50 @@
 class Factory():
   def ___init___(.self):
-    self.pos_x
-    self.pos_y
-    self.effective
-    self.type
-    self.level
+
+    self.Squares = [] #список занимемых квадратов. в СвЕТОВЫХ КОДАХ [[001, 001, 001],[002, 001, 001]] смотри доску в https://miro.com/app/board/uXjVLNKZfEk=/
+    self.Area = self.Pos_x 
+    
+    self.Effective
+    
+    self.Type
+    
+    self.Level #от 1 до 10
+    
+    self.StockCapacityExport
+    self.StockCapacityMaxExport
+    #Импорт добавляем понадобномти
+    
+    self.EfficiencyLossRate
+    
+    self.EfficiencyLossCounter
+    
+  def EfficiencyLoss(): #потеря эффективность когда счетчик EfficiencyLossCounter заполняеться!
+    self.EfficiencyLossCounter += 1
+    if self.EfficiencyLossCounter >= self.EfficiencyLossRate:
+      self.Effective -= 0.01
+
+
+
 
 class FarmFactory(Factory):
   def ___init___(.self):
-    self.
+    self.SoilQuality
+    
+    self.StockCapacityMaxExport = 24 * self.level
+    self.EfficiencyLossRate = 10 # недель это для теста
+
+class IronMineFactory(Factory):
+  def ___init___(.self):
+    self.SoilQuality
+    
+    self.StockCapacityMaxExport = 50 * self.level
+    self.EfficiencyLossRate = 10 # недель это для теста
+    
+
+
+
+
+
+
+
+
